bb7f7d48aa0155f6c8c1176d8c68b99bfc609438
Dinesh13794 <13794dinesh@gmail.com>
2025-02-19 09:44:49 +0530

    renamed url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173' 
+    origins 'https://13794taskmanagement.netlify.app/' 
 
     resource '*',
       headers: :any,
